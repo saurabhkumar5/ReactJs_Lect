cefee85c7a72ff1034e76625dc85532885eb94bf
saurabh kumar <saurabhkumar9493@gmail.com>
2023-08-01 22:40:38 +0530

    react-2

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-// function App() {
-//   return <ul>
-//     <li className='ruby'>sumit</li>
-//     <li className='luby'>saurabh</li>
-//     <li className='ruby'>ravi</li>
-//     <li className='ruby'>subham</li>
-//   </ul>
-// }
-
-// function App() {
-//   const names = ["sumit","saurabh","annu","anamika"]
-//   return <ul>
-//     {names.map(name=>{
-//       return <li className='ruby'>{name}</li>
-//     })}
-//   </ul>
-// }
-
+import Box from './components/Box'
 
 function App() {
-  const days = [{css:'ruby',data:'maonday'},{css:'luby',data:'sunday'}]
-  return <ul>
-    {days.map(name=>{
-      return <li className={name.css}>{name.data}</li>
-    })}
-  </ul>
-}
-
+  return <div>
+    <Box/>
+    {/* <Box/> */}
+  </div>
+  }
 export default App;
---
+++ src/components/BoxImage.js
@@ -0,0 +1,7 @@
+
+
+function BoxImage(){
+    return <img width="90px" height="90px" src="https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100"/>
+}
+
+export default BoxImage;
